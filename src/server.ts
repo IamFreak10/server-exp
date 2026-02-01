f2adeedbb0d0a4ad89ca47557f5e98bdfc7e11d9
IamFreak10 <user.email>
2026-02-01 04:48:35 +0000

    Added todos crud

--- src/server.ts
+++ src/server.ts
@@ -147,31 +147,52 @@ app.delete('/users/:id', async (req: Request, res: Response) => {
   }
 });
 
-app.post('/todos', async (req: Request, res: Response) => {
-  const { title, description } = req.body;
+// todos crud
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+
   try {
     const result = await pool.query(
-      `INSERT INTO todos (title,description) VALUES ($1,$2) RETURNING *`,
-      [title, description]
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title]
     );
-    if (result.rows.length === 0) {
-      res.status(404).json({
-        success: false,
-        message: 'User Not found',
-      });
-    } else {
-      res.status(200).json({
-        success: true,
-        message: 'User updated successfully',
-        data: result.rows[0],
-      });
-    }
-  } catch (e: any) {
+    res.status(201).json({
+      success: true,
+      message: "Todo created",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
     res.status(500).json({
       success: false,
-      message: e.message,
+      message: err.message,
     });
   }
+});
+
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+
+    res.status(200).json({
+      success: true,
+      message: "todos retrieved successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      datails: err,
+    });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
+  });
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
